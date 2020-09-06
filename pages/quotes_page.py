5042b7de770775896d87e24376ece0cadfa80139
Titan <sahilahuja1911@gmail.com>
2020-09-06 19:50:09 +0530

    In progress

--- pages/quotes_page.py
+++ pages/quotes_page.py
@@ -1,13 +1,13 @@
-from bs4 import BeautifulSoup
+
 from parsers.quote import QuoteParser
 from locators.quote_page_locators import QuotePageLocators
 
 class QuotesPage:
-    def __init__(self,page):
-        self.soup = BeautifulSoup(page, 'html.parser')
+    def __init__(self,browser):
+        self.browser = browser
         
     @property
     def quotes(self):
         locator = QuotePageLocators.QUOTE
-        quote_tags = self.soup.select(locator)
+        quote_tags = self.browser.find_elements_by_css_selector(locator)
         return [QuoteParser(e) for e in quote_tags]
